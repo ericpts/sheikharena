18ab32fa7a7d18fba33e54cef9b71b412bb46778
Eric Peter Stavarache <ericptst@gmail.com>
2015-08-01 14:10:31 +0300

    Added some models to the home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Test(models.Model):
+    inFile = models.FileField()
+    outFile = models.FileField()
+    score = models.IntegerField()
+
+
+class Problem(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200, primary_key=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
